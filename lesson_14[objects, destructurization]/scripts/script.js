f683c5dc40f23581f9c2324ffdf6f9e485683bc6
fr13n8 <ovsepanrik@gmail.com>
2023-05-10 13:51:56 +0300

    Add lesson 14

--- lesson_14[objects, destructurization]/scripts/script.js
+++ lesson_14[objects, destructurization]/scripts/script.js
@@ -61,38 +61,38 @@
 // }
 
 // Создать массив из объектов продуктов (свойства title, price, type) и, пройдясь циклом по массиву, сформировать строки. Вывести их в консоль.
-// const products = [
-//     {
-//         title: "Lenovo",
-//         price: 3000,
-//         type: "Laptop",
-//         discount: 25
-//     },
-//     {
-//         title: "Blender",
-//         price: 1500,
-//         type: "Electronics",
-//         discount: 13
-//     },
-//     {
-//         title: "Bicycle",
-//         price: 2000,
-//         type: "Sport",
-//         discount: 17
-//     },
-//     {
-//         title: "Qwerty",
-//         price: 100,
-//         type: "Notebook",
-//         discount: 8
-//     },
-//     {
-//         title: "Nvidia",
-//         price: 20000,
-//         type: "Electronics",
-//         discount: 10
-//     }
-// ]
+const products = [
+    {
+        title: "Lenovo",
+        price: 3000,
+        type: "Laptop",
+        discount: 25
+    },
+    {
+        title: "Blender",
+        price: 1500,
+        type: "Electronics",
+        discount: 13
+    },
+    {
+        title: "Bicycle",
+        price: 2000,
+        type: "Sport",
+        discount: 17
+    },
+    {
+        title: "Qwerty",
+        price: 100,
+        type: "Notebook",
+        discount: 8
+    },
+    {
+        title: "Nvidia",
+        price: 20000,
+        type: "Electronics",
+        discount: 10
+    }
+]
 // for (let i = 0; i < products.length; i++) {
 //     const {title, price, type, discount} = products[i]
 //     console.log(`Title of product: ${title}`);
@@ -115,3 +115,33 @@
 //         console.log(`Title of product: ${products[i].title}`);
 //     }
 // }
+
+// Вывести названия товаров и цену со скидкой. В процессе решения задачи используйте деструктурирующее присваивание
+// price - price * discount / 100
+// for (let i = 0; i < products.length; i++) {
+//     const {title:productName, discount, price} = products[i]
+//     console.log(`Product name: ${productName}`)
+//     console.log(`Product original price: ${price}. Discount: ${discount}`)
+//     console.log(`Product price after discount: ${price - price * discount / 100}`)
+//     console.log('============')
+// }
+
+// Пройтись циклом по массиву и вывести только спортивные товары(type, Sport).
+for (let i = 0; i < products.length; i++) {
+    const {title, price, type, discount} = products[i]
+    if(type === "Sport") {
+        console.log(`Title of product: ${title}`);
+        console.log(`Price of product: ${price}`);
+        console.log(`Type of product: ${type}`);
+        console.log(`Discount of product: ${discount}`);
+    }
+}
+
+// Создать новый пустой массив. Пройтись циклом по массиву с продуктами и в новый массив добавить те продукты, которые дешевле 15000.
+const newProducts = []
+for (let i = 0; i < products.length; i++) {
+    if (products[i].price < 15000) {
+        newProducts.push(products[i])
+    }
+}
+console.log(newProducts);
