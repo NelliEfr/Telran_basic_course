dba44457e9c7a47e121bf00eabcdd1d363c4fa75
fr13n8 <ovsepanrik@gmail.com>
2023-05-15 13:46:33 +0300

    Add lesson 17

--- lesson_17[practice, tasks]/scripts/script.js
+++ lesson_17[practice, tasks]/scripts/script.js
@@ -109,3 +109,122 @@
 // }
 // console.log(reverse("word"))
 // console.log(reverse("hello"))
+
+// Напишите функцию, которая в качестве аргументов получает слово и определяет, является ли оно палиндромом. Если да, функция возвращает true, в ином случае false.
+// function isPalindrome(str) {
+//     str = str.toLowerCase()
+//     let reversed = ''
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i]
+//     }
+//     return reversed === str
+// }
+// console.log(isPalindrome('hello'))
+// console.log(isPalindrome('anna'))
+// console.log(isPalindrome('Anna'))
+
+// Используя цикл, выведите в консоль сумму чисел из диапазона значений от 0 до 50 кратные 5.
+// function getSum() {
+//     let sum = 0
+//     // 1
+//     // for (let i = 0; i <= 50; i++) {
+//     //     if (i % 5 === 0) {
+//     //         sum += i
+//     //     }
+//     // }
+//     // 2
+//     // 0 5 10 15 20 25 30 ...
+//     for (let i = 0; i <= 50; i += 5) {
+//        sum += i 
+//     }
+//     return sum
+// }
+// console.log(getSum());
+
+// В программе объявлена переменная list, содержащая массив данных. Используя цикл, посчитайте сумму значений до первого булевого значения. Дойдя до булевого значения остановите цикл. Результат суммы выведите в консоль.
+// function sumOfArray() {
+//     const list = [1, 3, 8, true, 6, false, 69, 62, true]
+//     let sum = 0
+//     for (let i = 0; i < list.length; i++) {
+//         // 1
+//         // if(typeof list[i] !== 'boolean') { // 1[number] 3[number] 8[number] true[boolean]
+//         //     sum += list[i]
+//         // } else {
+//         //     return sum
+//         // }
+
+//         // 2
+//         if(typeof list[i] === 'boolean') {
+//             return sum
+//         }
+//         sum += list[i]
+//     }
+// }
+// console.log(sumOfArray())
+
+// В следующих задачах будет использоваться массив из объектов, каждый из которых описывает определенный товар:
+// const products = [
+//     {
+//         id: 1,
+//         title: "Lenovo",
+//         price: 3000,
+//         type: "Laptop",
+//         discount: 25
+//     },
+//     {
+//         id: 2,
+//         title: "Blender",
+//         price: 1500,
+//         type: "Electronics",
+//         discount: 13
+//     },
+//     {
+//         id: 3,
+//         title: "Bicycle",
+//         price: 2000,
+//         type: "Sport",
+//         discount: 17
+//     },
+//     {
+//         id: 4,
+//         title: "Qwerty",
+//         price: 100,
+//         type: "Notebook",
+//         discount: 8
+//     },
+//     {
+//         id: 5,
+//         title: "Nvidia",
+//         price: 20000,
+//         type: "Electronics",
+//         discount: 10
+//     }
+// ]
+// Написать цикл, который выводит только названия товаров
+// for (let i = 0; i < products.length; i++) {
+//     console.log(`Product title: ${products[i].title}`)
+// }
+
+// Написать цикл, который выводит для каждого товара строку по маске “<название> (<цена>)”
+// for (let i = 0; i < products.length; i++) {
+//     const {title, price} = products[i]
+//     console.log(`${title} (${price})`)
+// }
+
+// Написать цикл, который выводит для каждого товара строку по маске “<название> (<цена со скидкой>)”
+// for (let i = 0; i < products.length; i++) {
+//     const {title, price, discount} = products[i]
+//     console.log(`${title} (${price - price * discount / 100})`)
+// }
+
+// Написать функцию, которая получает в качестве аргумента массив из объектов и возвращает объект с самым дорогим товаром (без учета скидки).
+// function getExpensive(products) {
+//     let expensiveProduct = products[0]
+//     for (let i = 0; i < products.length; i++) {
+//         if(expensiveProduct.price < products[i].price) {
+//             expensiveProduct = products[i]
+//         }
+//     }
+//     return expensiveProduct
+// }
+// console.log(getExpensive(products))
