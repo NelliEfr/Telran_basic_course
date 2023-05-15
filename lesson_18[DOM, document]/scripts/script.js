0557b0b27e57e76c0633c6ae3918066caa7586d6
fr13n8 <ovsepanrik@gmail.com>
2023-05-15 13:48:53 +0300

    Add lesson 18

---
+++ lesson_18[DOM, document]/scripts/script.js
@@ -0,0 +1,24 @@
+// const paragraph = document.querySelector("p")
+// console.log(paragraph)
+// console.log(paragraph.innerText)
+// paragraph.innerText = "Hello"
+
+// const paragraphClass = document.querySelector(".my-paragraph")
+// console.log(paragraphClass)
+// paragraphClass.innerText = "World"
+
+// const paragraphs = document.querySelectorAll("p")
+// for (let i = 0; i < paragraphs.length; i++) {
+//     console.log(paragraphs[i]);
+// }
+
+// Написать программу, которая находит параграфы по классу и заменяет в каждом из них текст на “привет”.
+// const paragraphs = document.querySelectorAll(".my-paragraph")
+// console.log(paragraphs);
+// for (let i = 0; i < paragraphs.length; i++) {
+//     paragraphs[i].innerText = "привет"
+// }
+
+// Найти элемент с классом main который находится внутри box и записать в него текст “Я главный элемент”.
+// const mainElem = document.querySelector(".box .main")
+// mainElem.innerText = "“Я главный элемент”"
